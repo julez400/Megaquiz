bd43cae7bbb2d4a993106480e5088b2e4d18d547
julez400 <julian.r.wittmann@gmail.com>
2022-07-07 09:47:19 +0200

    Main page

--- app/src/main/java/com/zli/megaquiz/MainActivity.java
+++ app/src/main/java/com/zli/megaquiz/MainActivity.java
@@ -2,13 +2,24 @@ package com.zli.megaquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(view -> {
+            Intent resultIntent = new Intent(this, QuestionActivity.class);
+            startActivity(resultIntent);
+        });
     }
 }
